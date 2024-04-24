79b5efeda5bb35450b12c68048b856b51a0c118b
Luise Haeuser <haeusele@hitssv566.villa-bosch.de>
2024-04-24 08:49:08 +0000

    Make experiment work

--- experiment.py
+++ experiment.py
@@ -5,8 +5,7 @@ from tabulate import tabulate
 from lingdata.categorical import CategoricalData
 
 msa_types = ["bin", "catg_bin", "catg_multi"]
-msa_types = ["bin"]
-file_ending = {"bin" : ".phy", "catg_bin" + ".catg", "catg_multi" + ".catg"}
+file_ending = {"bin" : ".phy", "catg_bin":  ".catg", "catg_multi" : ".catg"}
 msa_dir = "data/msa"
 raxml_dir = "data/raxml-ng"
 input_dir = "data/simulated_data"
@@ -21,7 +20,7 @@ def run_inference(msa_path, model, prefix, args = ""):
     prefix_dir = "/".join(prefix.split("/")[:-1])
     if not os.path.isdir(prefix_dir):
         os.makedirs(prefix_dir)
-    if not os.path.isfile(best_tree_path(prefix)):
+    if not os.path.isfile(prefix + ".raxml.bestTree"):
         args = args + " --redo"
     command = "./bin/raxml-ng"
     command += " --msa " + msa_path
@@ -40,7 +39,7 @@ def rf_distance(t1, t2):
     rf, max_rf, common_leaves, parts_t1, parts_t2,discard_t1, discart_t2 = t1.robinson_foulds(t2, unrooted_trees = True)
     if max_rf == 0:
         return float('nan')
-    return rf/max_rf * 100
+    return (rf/max_rf) * 100
 
 def generate_msas():
     msa_dir = "data/msa"
@@ -59,7 +58,7 @@ def generate_msas():
                 cd.write_msa(msa_file_name, msa_type)
             max_values_dict[input_file] = cd.max_values()
         max_values_path = os.path.join(msa_dir, borrowing_mode, "max_values.csv")
-        json.dump(d, open(max_values_path,'w+'))
+        json.dump(max_values_dict, open(max_values_path,'w+'))
 
 
 def run_raxml_ng():
@@ -77,16 +76,16 @@ def run_raxml_ng():
             prefix = os.path.join(raxml_dir, borrowing_mode, input_file.split('.')[0], msa_type)
             run_inference(msa_file_name, "BIN+G", prefix)
 
-            #msa_type = "catg_bin"
-            #msa_file_name = os.path.join(current_msa_dir, msa_type + file_ending[msa_type])
-            #prefix = os.path.join(raxml_dir, borrowing_mode, input_file.split('.')[0], msa_type)
-            #run_inference(msa_file_name, "BIN+G", prefix, "--prob-msa on")
+            msa_type = "catg_bin"
+            msa_file_name = os.path.join(current_msa_dir, msa_type + file_ending[msa_type])
+            prefix = os.path.join(raxml_dir, borrowing_mode, input_file.split('.')[0], msa_type)
+            run_inference(msa_file_name, "BIN+G", prefix, "--prob-msa on")
 
-            #msa_type = "catg_multi"
-            #msa_file_name = os.path.join(current_msa_dir, msa_type + file_ending[msa_type])
-            #prefix = os.path.join(raxml_dir, borrowing_mode, input_file.split('.')[0], msa_type)
-            #x = max_values_dict[input_file]
-            #run_inference(msa_file_name, "MULTI" + str(x) + "_MK+G", prefix, "--prob-msa on")
+            msa_type = "catg_multi"
+            msa_file_name = os.path.join(current_msa_dir, msa_type + file_ending[msa_type])
+            prefix = os.path.join(raxml_dir, borrowing_mode, input_file.split('.')[0], msa_type)
+            x = max_values_dict[input_file]
+            run_inference(msa_file_name, "MULTI" + str(x) + "_MK+G", prefix, "--prob-msa on")
 
 
 def evaluate_trees():
@@ -96,7 +95,7 @@ def evaluate_trees():
     no_borrowing_trees = [Tree(line) for line in lines]
     results = [{} for _ in range(4)]
     for borrowing_mode in os.listdir(input_dir):
-        poly_level = borrowing_mode,split("_")[0]
+        poly_level = borrowing_mode.split("_")[0]
         for i in range(4):
             if poly_level not in results[i]:
                 results[i][poly_level] = dict([(msa_type, []) for msa_type in msa_types])
@@ -104,14 +103,14 @@ def evaluate_trees():
         max_values_path = os.path.join(msa_dir, borrowing_mode, "max_values.csv")
         max_values_dict = json.load(open(max_values_path))
         for input_file in os.listdir(borrowing_dir):
-            if borrowing_mode,split("_")[1] == "noborrowing":
-                idx = int(input_file.split("_")[1].split("tree")[0]) - 1
-                reference_tree = no_borrowing_trees[idx]
+            if borrowing_mode.split("_")[1] == "noborrowing":
                 borrowing_edges = 0
+                idx = int(input_file.split("_")[1].split("tree")[1]) - 1
             else:
-                tree_name = input_file.split("_")[1] + ".txt"
-                reference_tree = Tree(os.path.join(reference_trees_dir, tree_name))
-                borrowing_edges = int(input_file.split("_")[1].split("-")[0].split("net")[0])
+                print(input_file)
+                idx = int(input_file.split("_")[1].split("-")[1]) -1
+                borrowing_edges = int(input_file.split("_")[1].split("-")[0].split("net")[1])
+            reference_tree = no_borrowing_trees[idx]
             for msa_type in msa_types:
                 prefix = os.path.join(raxml_dir, borrowing_mode, input_file.split('.')[0], msa_type)
                 ml_trees_path = prefix + ".raxml.mlTrees"
@@ -124,7 +123,7 @@ def evaluate_trees():
                 rf_distances = [rf_distance(ml_tree, reference_tree) for ml_tree in ml_trees]
                 results[borrowing_edges][poly_level][msa_type].append(sum(rf_distances) / len(rf_distances))
 
-    headers = []"poly_level"] + msa_types
+    headers = ["poly_level"] + msa_types
     for borrowing_edges in range(4):
         r = []
         for poly_level in results[borrowing_edges]:
@@ -135,3 +134,8 @@ def evaluate_trees():
             r.append(row)
         print("Borrowing edges:", str(borrowing_edges))
         print(tabulate(r, tablefmt="pipe", headers = headers))
+
+
+generate_msas()
+run_raxml_ng()
+evaluate_trees()
